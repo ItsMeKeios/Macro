3fa80bfd94e0bbaa8e29378528accfc4bde890e0
ItsMeKeios <112435950+ItsMeKeios@users.noreply.github.com>
2024-02-08 16:20:17 -0600

    Add files via upload

---
+++ random.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.querySelector('.button-random-move');
+    var areaWidth = 300; // Ancho del área permitida
+    var areaHeight = 200; // Alto del área permitida
+  
+    function moveButton() {
+      var maxX = areaWidth - button.offsetWidth;
+      var maxY = areaHeight - button.offsetHeight;
+  
+      var randomX = Math.floor(Math.random() * maxX);
+      var randomY = Math.floor(Math.random() * maxY);
+  
+      button.style.transition = "transform 0s"; // Hacer que la transición sea instantánea
+      button.style.transform = 'translate(' + randomX + 'px, ' + randomY + 'px)';
+    }
+  
+    function showAlert() {
+      alert("FAKIU, COMO POR QUE LE PICAS A QUE NO QUIERES????>:((");
+    }
+  
+    button.addEventListener('mouseover', moveButton);
+    button.addEventListener('click', showAlert);
+  });
+  
